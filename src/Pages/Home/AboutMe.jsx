18df192f095359764976ac33cd85929347456d35
adillaghfour <126948485+adillaghfour@users.noreply.github.com>
2024-05-23 17:36:04 +0200

    Add files via upload

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Je suis un développeur full stack compétent dans les technologies front-end et back-end. Mon expertise comprend : <br />
+
+Front-End : HTML, CSS, JavaScript, React, Angular, Vue.js
+Back-End : Node.js, Python, Ruby, PHP, Express, Django, Ruby on Rails, Laravel
+Bases de données : MySQL, PostgreSQL, MongoDB
+DevOps : Git, Docker, AWS, pipelines CI/CD <br />
+J'ai créé des plateformes de commerce électronique, des systèmes CMS et des applications de médias sociaux. Mon objectif est de créer des applications Web efficaces, évolutives et conviviales. Passionné par l'apprentissage continu et la résolution de problèmes, je privilégie une intégration transparente et une excellente expérience utilisateur dans tous mes projets.
+          </p>
+          <p className="hero--section-description">
+
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Adil Laghfour</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Conception et développement de l'interface utilisateur (front-end) à l'aide de techniques telles que (HTML CSS JavaScript React...),
+            <br /> Développer des services et des interfaces (back-end) en utilisant des langages de programmation tels que (Python, Node.js, etc).
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
